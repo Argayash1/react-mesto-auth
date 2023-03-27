a8f3771fbb609e4845b0a2d52883fb555e193f43
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-03-27 18:40:11 +0500

    Доделал Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,42 @@
+import { Routes, Route, NavLink } from "react-router-dom";
 import logo from "../images/header-logo.svg";
 
 function Header({ loggedIn, onSignOut, userEmail }) {
-  const navElement = loggedIn ? (
-    <button onClick={onSignOut} className="header__button">
-      Выйти
-    </button>
-  ) : (
-    <p>Войти</p>
-  );
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Логотип" />
       <nav>
-        <span>{userEmail}</span>
-        {navElement}
+        <Routes>
+          {loggedIn && (
+            <Route
+              path="/"
+              element={
+                <>
+                  <span>{userEmail}</span>
+                  <button onClick={onSignOut} className="header__button">
+                    Выйти
+                  </button>
+                </>
+              }
+            />
+          )}
+          <Route
+            path="/sign-in"
+            element={
+              <NavLink className="header__navlink" to="/sign-up">
+                Регистрация
+              </NavLink>
+            }
+          />
+          <Route
+            path="/sign-up"
+            element={
+              <NavLink className="header__navlink" to="/sign-in">
+                Войти
+              </NavLink>
+            }
+          />
+        </Routes>
       </nav>
     </header>
   );
