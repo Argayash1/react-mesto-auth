d7452d4f6acdd656dc786bf61e459621cac7f660
Yakov Zilberman <zilberman.yakoff@yandex.ru>
2023-04-02 23:51:06 +0500

    Исправил анимацию

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,21 +4,21 @@ import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 function Navbar({ loggedIn, onSignOut, userEmail, isOpen, onMobileMenu, onClose, isLoading }) {
   return (
     <nav className="navbar">
-      <ul className="header__navlist">
+      <ul className="navbar__navlist">
         <Routes>
           {loggedIn && !isLoading && (
             <Route
               path="/"
               element={
-                <li className="header__navlist-item">
-                  <div className={`header__navlist-elements ${isOpen && "header__navlist-elements_is-active"}`}>
-                    <span className="header__usermail">{userEmail}</span>
-                    <button onClick={onSignOut} className="header__button">
+                <li className="navbar__navlist-item">
+                  <div className={`navbar__navlist-elements ${isOpen && "navbar__navlist-elements_is-active"}`}>
+                    <span className="navbar__usermail">{userEmail}</span>
+                    <button onClick={onSignOut} className="navbar__button">
                       Выйти
                     </button>
                   </div>
-                  {!isOpen && <AiOutlineMenu className="header__navlist-button" size="30" onClick={onMobileMenu} />}
-                  {isOpen && <AiOutlineClose className="header__navlist-button" size="30" onClick={onClose} />}
+                  {!isOpen && <AiOutlineMenu className="navbar__navlist-button" size="30" onClick={onMobileMenu} />}
+                  {isOpen && <AiOutlineClose className="navbar__navlist-button" size="30" onClick={onClose} />}
                 </li>
               }
             />
@@ -26,8 +26,8 @@ function Navbar({ loggedIn, onSignOut, userEmail, isOpen, onMobileMenu, onClose,
           <Route
             path="/sign-in"
             element={
-              <li className="header__navlist-item">
-                <NavLink className="header__navlink" to="/sign-up">
+              <li className="navbar__navlist-item">
+                <NavLink className="navbar__navlink" to="/sign-up">
                   Регистрация
                 </NavLink>
               </li>
@@ -36,8 +36,8 @@ function Navbar({ loggedIn, onSignOut, userEmail, isOpen, onMobileMenu, onClose,
           <Route
             path="/sign-up"
             element={
-              <li className="header__navlist-item">
-                <NavLink className="header__navlink" to="/sign-in">
+              <li className="navbar__navlist-item">
+                <NavLink className="navbar__navlink" to="/sign-in">
                   Войти
                 </NavLink>
               </li>
